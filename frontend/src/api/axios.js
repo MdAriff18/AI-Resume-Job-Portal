a2ad097857a803dba39c9da3dcf4381242c17c48
MdAriff18 <mohammedarif3416@gmail.com>
2026-08-04 14:51:37 +0530

    Fix axios base URL

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,13 +1,12 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: '$ {import.meta.env.VITE_API_URL} / api',
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
 });
 
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem("access");
 
-  // Register & Login requests ki token pampakudadhu
   if (
     token &&
     !config.url.includes("login") &&
